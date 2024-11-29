71871dbdda8e5c846f34b8b0d33190ff9cce1ad1
stefin <stefinracho@gmail.com>
2024-11-29 03:18:35 -0800

    refactor none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,10 +6,6 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.cppcheck,
       },
     })
 
