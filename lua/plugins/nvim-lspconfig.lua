e0a03ec155ed28ae0696ec654b3ea00036488910
stefin <stefinracho@gmail.com>
2025-01-01 21:17:45 -0800

    fix: clangd fallback to webkit

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,67 +1,67 @@
 -- LSP Support
 return {
-  -- LSP Configuration
-  -- https://github.com/neovim/nvim-lspconfig
-  "neovim/nvim-lspconfig",
-  event = "VeryLazy",
-  dependencies = {
-    -- LSP Management
-    -- https://github.com/williamboman/mason.nvim
-    { "williamboman/mason.nvim" },
-    -- https://github.com/williamboman/mason-lspconfig.nvim
-    { "williamboman/mason-lspconfig.nvim" },
+    -- LSP Configuration
+    -- https://github.com/neovim/nvim-lspconfig
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    dependencies = {
+        -- LSP Management
+        -- https://github.com/williamboman/mason.nvim
+        { "williamboman/mason.nvim" },
+        -- https://github.com/williamboman/mason-lspconfig.nvim
+        { "williamboman/mason-lspconfig.nvim" },
 
-    -- Useful status updates for LSP
-    -- https://github.com/j-hui/fidget.nvim
-    { "j-hui/fidget.nvim",                opts = {} },
+        -- Useful status updates for LSP
+        -- https://github.com/j-hui/fidget.nvim
+        { "j-hui/fidget.nvim",                opts = {} },
 
-    -- Additional lua configuration, makes nvim stuff amazing!
-    -- https://github.com/folke/neodev.nvim
-    { "folke/neodev.nvim" },
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "clangd",
-        "cssls",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "pylsp",
-        "tailwindcss",
-        "ts_ls",
-      },
-    })
-
-    local lspconfig = require("lspconfig")
-    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local lsp_attach = function(client, bufnr)
-      -- Create your keybindings here...
-    end
-
-    -- Call setup on each LSP server
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          on_attach = lsp_attach,
-          capabilities = lsp_capabilities,
+        -- Additional lua configuration, makes nvim stuff amazing!
+        -- https://github.com/folke/neodev.nvim
+        { "folke/neodev.nvim" },
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "clangd",
+                "cssls",
+                "html",
+                "jsonls",
+                "lua_ls",
+                "pylsp",
+                "tailwindcss",
+                "ts_ls",
+            },
         })
-      end,
-    })
 
-    -- Lua LSP settings
-    lspconfig.lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { "vim" },
-          },
-        },
-      },
-    })
-    lspconfig.clangd.setup({ autostart = true })
-    lspconfig.pylsp.setup({ autostart = true })
-  end,
+        local lspconfig = require("lspconfig")
+        local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lsp_attach = function(client, bufnr)
+            -- Create your keybindings here...
+        end
+
+        -- Call setup on each LSP server
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    on_attach = lsp_attach,
+                    capabilities = lsp_capabilities,
+                })
+            end,
+        })
+
+        -- Lua LSP settings
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+        lspconfig.clangd.setup({ autostart = true, cmd = { "clangd", "--fallback-style=webkit" } })
+        lspconfig.pylsp.setup({ autostart = true })
+    end,
 }
